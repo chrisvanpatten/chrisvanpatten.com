23375c7b262891f3baefc88fe05dd5840e16c8d7
Chris Van Patten <chris@vanpattenmedia.com>
2025-04-15 16:00:38 -0400

    add: backup script

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ source ./chrisvanpatten-mysql/.env
 export MYSQL_PWD=$MYSQL_ROOT_PASSWORD
 
 # Start the Fly.io proxy
-flyctl proxy 13306:3306 -a chrisvanpatten-mysql &
+fly proxy 13306:3306 -a chrisvanpatten-mysql &
 PROXY_PID=$!
 
 # Wait for the proxy to connect
@@ -16,7 +16,12 @@ sleep 4
 
 # Dump the database
 export DATETIME=$(date +"%Y%m%d%H%M")
-$MYSQLDUMP_LOCATION -u root -h 127.0.0.1 -P 13306 ghost | gzip > ghost-$DATETIME.sql.gz
+$MYSQLDUMP_LOCATION -u root -h 127.0.0.1 -P 13306 ghost | gzip > backups/database-$DATETIME.sql.gz
 
 # Kill the proxy command
 kill "$PROXY_PID"
+
+fly ssh console -a chrisvanpatten -C "tar -czf files-$DATETIME.tar.gz content/images"
+cd backups
+fly sftp get /var/lib/ghost/files-$DATETIME.tar.gz -a chrisvanpatten
+fly ssh console -a chrisvanpatten -C "rm files-$DATETIME.tar.gz"
